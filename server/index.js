7fb53ccc64f8ffd6eaee437c9d8b58cc904a59f2
Reese Schumacher <reesesch1@gmail.com>
2021-07-15 12:54:07 -0600

    v1.2

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use(express.static('public'))
+
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 const port = process.env.PORT || 4005
