2fd9db229eaff3fe6fa0e7d53a5b4d5dc5f77071
Reese Schumacher <reesesch1@gmail.com>
2021-07-15 11:16:15 -0600

    v4

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-const port = process.env.port
+const port = process.env.PORT || 4005
 
 app.listen(port, () => {
     console.log(`Running on ${port}! :)`)
